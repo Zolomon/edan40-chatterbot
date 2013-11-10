cc73e5d6b1b5855f29963362f5da83df59d2f9d2
zol <zolomon@gmail.com>
2013-11-10 21:06:59 +0100

    Remove import

--- Pattern.hs
+++ Pattern.hs
@@ -1,6 +1,6 @@
 module Pattern where
 import Utilities
-import Data.Maybe
+
 -------------------------------------------------------
 -- Match and substitute
 --------------------------------------------------------
